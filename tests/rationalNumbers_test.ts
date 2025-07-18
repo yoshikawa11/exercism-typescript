ef980a85bfd505db4527247440d6258937164f21
yoshikawa masashi <gunterriote@yahoo.co.jp>
2025-07-18 20:37:27 +0900

    ファイル名の変更

--- tests/rationalNumbers_test.ts
+++ tests/rationalNumbers_test.ts
@@ -2,7 +2,7 @@ import {
   assertAlmostEquals,
   assertEquals,
 } from "https://deno.land/std@0.224.0/assert/mod.ts";
-import { Rational } from "../answers/rational.ts";
+import { Rational } from "../answers/rationalNumbers.ts";
 
 Deno.test("Addition", () => {
   assertEquals(new Rational(1, 2).add(new Rational(1, 2)), new Rational(1, 1));
